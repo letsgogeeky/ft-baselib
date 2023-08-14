008ba9a766b4648e45f3e7523a45bf0de5999cb5
Ramy <ramymohamedmousa@gmail.com>
2023-08-14 02:45:45 +0200

    minor fix for str arr free function

--- libft/ra_array.c
+++ libft/ra_array.c
@@ -6,7 +6,7 @@
 /*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 22:58:49 by ramoussa          #+#    #+#             */
-/*   Updated: 2023/08/12 23:08:53 by ramoussa         ###   ########.fr       */
+/*   Updated: 2023/08/14 02:25:56 by ramoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@ void	str_arr_free(char **arr)
 	int	idx;
 
 	idx = 0;
-	while (arr && arr[idx])
+	while (arr && arr[idx] != 0)
 	{
 		free(arr[idx]);
 	}
+	free(arr);
 }
